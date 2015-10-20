8c3a0aa7a43e39e3a2504419984abe6277e6cc36
Marek Czernohous <marek@czernohous.de>
2015-10-20 14:42:11 +0200

    JFrameTest hinzugefügt

---
+++ src/JFrameTest.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+public class JFrameTest {
+
+	public static void main(String[] args) {
+		JFrame f=new JFrame("Hier könnte Ihre Werbung stehen");
+		f.setVisible(true);
+
+	}
+
+}

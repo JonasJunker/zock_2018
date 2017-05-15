0ffe9070df9964dbbc7394f2e71681c3cc089ba4
Marek.Czernohous <Marek.Czernohous@SK-D8CB8A4FAB08.2631.skool.net>
2017-05-15 14:52:55 +0200

    GeometricObject überladen

--- src/animationGame/GeometricObject.java
+++ src/animationGame/GeometricObject.java
@@ -7,13 +7,35 @@ public class GeometricObject {
 	public double height;
 	
 	
+	public GeometricObject(double height, double width, Vertex pos){
+		
+		this.height=height;
+		this.width=width;
+		this.pos=pos;
+	
+		
+		
+		if(height<0){
+			this.height=-height;
+			pos.y=pos.y-height;
+		}
+		
+				
+		if(width<0){
+			this.width=-width;
+			pos.x=pos.x-width;
+		}
+		
+	}
+	public GeometricObject(double height, double width){this(height, width, new Vertex(0,0));}
+		
+	
 	
 	public Vertex getPos(){return pos;}
 	
 	public double getHeight(){return height;}
 	
 	public double getWidth(){return width;}
-	
 	
 	
 	

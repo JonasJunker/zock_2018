7fbde67f698dac56e8567beb8a95b3e3491ec621
Marek.Czernohous <Marek.Czernohous@SK-D8CB8A4FAB08.2631.skool.net>
2017-05-15 14:27:33 +0200

    GeometricObject

---
+++ src/animationGame/GeometricObject.java
@@ -0,0 +1,20 @@
+package animationGame;
+
+public class GeometricObject {
+
+	public Vertex pos;
+	public double width;
+	public double height;
+	
+	
+	
+	public Vertex getPos(){return pos;}
+	
+	public double getHeight(){return height;}
+	
+	public double getWidth(){return width;}
+	
+	
+	
+	
+}
